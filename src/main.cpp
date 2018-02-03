4b776f84bc4113926a2b36120e047d27659e5473
Dirk-Jan Faber <d.faber@gmail.com>
2018-02-03 21:30:26 +0100

    Now actually tested with the Things Network http integration. This meant a minor change in allocated memory for json parsing (my first tests where with more limited sized json objects).

--- src/main.cpp
+++ src/main.cpp
@@ -63,10 +63,13 @@ void setup(void){
   });
   server.on("/ttn",HTTP_POST, [](){
     digitalWrite(BUILTIN_LED, LOW);
-    StaticJsonBuffer<200> newBuffer;
+    const size_t bufferSize = JSON_ARRAY_SIZE(1) + JSON_OBJECT_SIZE(5) + JSON_OBJECT_SIZE(7) + JSON_OBJECT_SIZE(9) + JSON_OBJECT_SIZE(10) + 720;
+    DynamicJsonBuffer newBuffer(bufferSize);
     JsonObject& root = newBuffer.parseObject(server.arg("plain"));
+    Serial.println(server.arg("plain"));
     // strip the payload_fields as body for the thingspeak request
     body = root["payload_fields"].as<String>();
+    Serial.println(body);
 
     http.begin("https://api.thingspeak.com/update.json", "78:60:18:44:81:35:BF:DF:77:84:D4:0A:22:0D:9B:4E:6C:DC:57:2C");
     http.addHeader("Content-Type", "application/json");
@@ -77,6 +80,7 @@ void setup(void){
 
     if ( httpCode > 0 ) {
       payload = http.getString();
+      Serial.println(payload);
       server.send ( 200, "text/json", payload);
     } else {
       String error = "{ \"error\" :";
@@ -89,7 +93,7 @@ void setup(void){
     digitalWrite(BUILTIN_LED, HIGH);
 
   });
-  
+
   server.begin();
   Serial.println("HTTP server started");
 }
