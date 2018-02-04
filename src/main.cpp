930987cb1d0e99b12ab287027b5301019e9ab479
Dirk-Jan Faber <d.faber@gmail.com>
2018-02-04 12:10:05 +0100

    Added support for over the air updating.

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,8 @@
 #include <WiFiClient.h>
 #include <ESP8266HTTPClient.h>
 
+#include <ArduinoOTA.h>
+
 MDNSResponder mdns;
 
 ESP8266WebServer server(80);
@@ -58,6 +60,26 @@ void setup(void){
     Serial.println("MDNS responder started");
   }
 
+  ArduinoOTA.onStart([]() {
+    Serial.println("OTA Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("OTA End");
+    Serial.println("Rebooting...");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r\n", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+
   server.on("/", [](){
     server.send(200, "text/html", webPage);
   });
@@ -99,6 +121,6 @@ void setup(void){
 }
 
 void loop(void){
+  ArduinoOTA.handle();
   server.handleClient();
-
 }
